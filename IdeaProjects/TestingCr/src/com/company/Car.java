a7ffec16ac45a5cb4564471a032d2ec40a6c8c6b
Karina <ruizkarina532@gmail.com>
2021-06-13 07:28:14 -0700

    Commit for the car tester

---
+++ IdeaProjects/TestingCr/src/com/company/Car.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Car {
+    //features we want in the car
+    String color;
+    String number;
+    String model;
+    String door;
+}
---
+++ IdeaProjects/TestingCr/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car audi = new Car(); //creating an obj of class Car
+        audi.color="black";
+        audi.model="R8";
+        audi.door="2";
+        audi.number="00790";
+    }
+}
